bab9c45e4cd30c46a097d4ffd3c0782f1a1b60ac
destinyarlotta <darlotta@ufl.edu>
2017-11-03 17:49:16 -0400

    Initial commit

---
+++ js/look.js
@@ -0,0 +1,31 @@
+var image1 = document.getElementById("image1");
+
+
+function change() {
+  // change the src attribute with setAttribute()
+  image1.setAttribute("src", "images/sunflower.jpg");
+// image1 = document.getElementById("image2");
+}
+
+document.getElementById("image1").onclick = change;
+
+
+function change2() {
+  // change the src attribute with setAttribute()
+  image2.setAttribute("src", "rose.jpg");
+// image1 = document.getElementById("image2");
+}
+
+document.getElementById("image2").onclick = change2;
+
+function change3() {
+  // change the src attribute with setAttribute()
+  image3.setAttribute("src", "hibiscus.jpg");
+// image1 = document.getElementById("image2");
+}
+
+document.getElementById("image3").onclick = change3;
+
+//document.getElementById("image2").onclick = change2;
+
+//document.getElementById("image1").onclick = change3;
